16787272fd7b384eafc34ad234db0c387bdcd13f
Pedro <pvpswapedro123@gmail.com>
2024-03-22 10:13:36 -0300

    first commit

--- src/app/firebase/index.ts
+++ src/app/firebase/index.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB2rKeXpyiOcBXZKnCuRyHtHYLaNavGefw",
-    authDomain: "upload-app-6796b.firebaseapp.com",
-    projectId: "upload-app-6796b",
-    storageBucket: "upload-app-6796b.appspot.com",
-    messagingSenderId: "856311796285",
-    appId: "1:856311796285:web:05ad7c56ce1cb4c9ca1054"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
